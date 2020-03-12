b0c9f563f4963e74f7bb95512a248b353c0b2622
janghyeon.jung <janghyeon.jung@lgepartner.com>
2020-03-12 12:46:59 +0900

    X-Frame 오류 관련 수정

--- src/main/java/kr/co/photograph/jhgallery/component/CustomFilter.java
+++ src/main/java/kr/co/photograph/jhgallery/component/CustomFilter.java
@@ -1,44 +1,44 @@
-package kr.co.photograph.jhgallery.component;
+//package kr.co.photograph.jhgallery.component;
+////
+////import lombok.AllArgsConstructor;
+////import org.apache.catalina.core.ApplicationContext;
+////import org.apache.tomcat.util.http.SameSiteCookies;
+////import org.hibernate.query.criteria.internal.expression.ConcatExpression;
+////import org.springframework.context.event.ContextRefreshedEvent;
+////import org.springframework.context.event.EventListener;
+////import org.springframework.session.web.http.CookieSerializer;
+////import org.springframework.session.web.http.DefaultCookieSerializer;
+////import org.springframework.stereotype.Component;
+////
+////@Component
+////@AllArgsConstructor
+////public class SameSiteInjector {
+////
+////    private final ApplicationContext applicationContext;
+////
+////    @EventListener
+////    public void onApplicationEvent(ContextRefreshedEvent event) {
+////        DefaultCookieSerializer cookieSerializer = applicationContext.
+////        cookieSerializer.setSameSite("None");
+////    }
+////}
 //
-//import lombok.AllArgsConstructor;
-//import org.apache.catalina.core.ApplicationContext;
-//import org.apache.tomcat.util.http.SameSiteCookies;
-//import org.hibernate.query.criteria.internal.expression.ConcatExpression;
-//import org.springframework.context.event.ContextRefreshedEvent;
-//import org.springframework.context.event.EventListener;
-//import org.springframework.session.web.http.CookieSerializer;
-//import org.springframework.session.web.http.DefaultCookieSerializer;
-//import org.springframework.stereotype.Component;
 //
-//@Component
-//@AllArgsConstructor
-//public class SameSiteInjector {
+//import org.springframework.web.filter.GenericFilterBean;
 //
-//    private final ApplicationContext applicationContext;
+//import javax.servlet.FilterChain;
+//import javax.servlet.ServletException;
+//import javax.servlet.ServletRequest;
+//import javax.servlet.ServletResponse;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
 //
-//    @EventListener
-//    public void onApplicationEvent(ContextRefreshedEvent event) {
-//        DefaultCookieSerializer cookieSerializer = applicationContext.
-//        cookieSerializer.setSameSite("None");
+//public class CustomFilter extends GenericFilterBean {
+//    @Override
+//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+//        HttpServletResponse resp = (HttpServletResponse)response;
+//        resp.setHeader("Set-Cookie", "locale=kr; HttpOnly; SameSite=None");
+//        resp.setHeader("X-Frame-Options", "ALLOW-FROM http://49.50.165.81/");
+//        chain.doFilter(request, response);
 //    }
 //}
-
-
-import org.springframework.web.filter.GenericFilterBean;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-public class CustomFilter extends GenericFilterBean {
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HttpServletResponse resp = (HttpServletResponse)response;
-        resp.setHeader("Set-Cookie", "locale=kr; HttpOnly; SameSite=None");
-        resp.setHeader("X-Frame-Options", "ALLOW-FROM http://49.50.165.81/");
-        chain.doFilter(request, response);
-    }
-}
