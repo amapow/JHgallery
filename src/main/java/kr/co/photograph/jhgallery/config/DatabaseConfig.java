53a5f346d919f08e9d1c2f2012a394d89a69af75
amapow <amapow25@gmail.com>
2020-03-14 19:42:13 +0900

    안쓰는 import 삭제

--- src/main/java/kr/co/photograph/jhgallery/config/DatabaseConfig.java
+++ src/main/java/kr/co/photograph/jhgallery/config/DatabaseConfig.java
@@ -1,21 +1,12 @@
 package kr.co.photograph.jhgallery.config;
-
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
-import java.sql.DriverManager;
 
 @EnableAutoConfiguration
 @EnableTransactionManagement
